9f317dc997a14cae88cf547d7a8de9a4758caf12
AkmaljonCoder <akmaljonazimjonov@gmail.com>
2022-07-28 11:42:16 +0500

    Akmaljon

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { Box, BoxWrapper, Container, ProjectImg, Title } from './style'
-import Project1 from '../../assets/Portfolio/project1.png'
+
+import CyberSport from '../../assets/Portfolio/cybersport.png'
+import EasyBank from '../../assets/Portfolio/easy-bank.png'
+import TodoList from '../../assets/Portfolio/todo-list.png'
+import Shop from '../../assets/Portfolio/shop.png'
 
 const Portfolio = () => {
   return (
@@ -10,23 +14,23 @@ const Portfolio = () => {
       <BoxWrapper>
 
         <Box>
-          <ProjectImg src={Project1} />
-          <a>Online fashion store - Homepage</a>
+          <ProjectImg src={CyberSport} />
+          <a onClick={()=>window.open('https://cyber-portfolio.vercel.app/')}>Cyber sport registration</a>
         </Box>
 
         <Box>
-          <ProjectImg src={Project1} />
-          <a>Online fashion store - Homepage</a>
+          <ProjectImg src={EasyBank} />
+          <a onClick={()=>window.open('https://easy-bank-project.vercel.app/')}>Easy bank project</a>
         </Box>
 
         <Box>
-          <ProjectImg src={Project1} />
-          <a>Online fashion store - Homepage</a>
+          <ProjectImg src={TodoList} />
+          <a onClick={()=>window.open('https://to-do-list-kappa-ten.vercel.app/')}>Todo list with react app</a>
         </Box>
 
         <Box>
-          <ProjectImg src={Project1} />
-          <a>Online fashion store - Homepage</a>
+          <ProjectImg src={Shop} />
+          <a onClick={()=>window.open('https://market-web-weld.vercel.app/')}>Online shop project</a>
         </Box>
 
       </BoxWrapper>
