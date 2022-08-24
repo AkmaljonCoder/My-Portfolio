4a08c1109d066bc03b789d66fed512b73f9b2f98
AzimovvCoder <azimovvcoder10@gmail.com>
2022-08-24 13:52:57 +0500

    akmal

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -5,6 +5,8 @@ import HtmlIc from '../../assets/Skills/html-5.png'
 import CssIc from '../../assets/Skills/css-3.png'
 import JsIc from '../../assets/Skills/js.png'
 import ReactIc from '../../assets/Skills/physics.png'
+import Bootstrap from '../../assets/Skills/bootstrap.png'
+import Github from '../../assets/Skills/github.png'
 
 const Skills = () => {
     
@@ -20,7 +22,15 @@ const Skills = () => {
           </Card>
           <Card>
             <img src={CssIc} alt="" />
-            <p>CSS</p>
+            <p>CSS , SASS</p>
+          </Card>
+          <Card>
+            <img src={Bootstrap} alt="" />
+            <p>Bootstrap 5</p>
+          </Card>
+          <Card>
+            <img src={Github} alt="" />
+            <p>Github</p>
           </Card>
           <Card>
             <img src={JsIc} alt="" />
