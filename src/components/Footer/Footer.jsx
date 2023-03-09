68c899eee3be99b63b8efb918a5de27c306a2b58
Akmaljon Azimov <96201968+AkmaljonCoder@users.noreply.github.com>
2023-03-09 22:56:11 +0500

    update footer
    social media links has changed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -17,9 +17,9 @@ const Footer = () => {
             </NetworkIcons>
             <LinkDiv>
                 <h2>Like me on</h2>
-                <p onClick={()=>window.open('https://twitter.com/azimov_akmaljon/')}>Twitter, </p>
-                <p onClick={()=>window.open('https://t.me/akmal_one')}>Telegram, </p>
-                <p onClick={()=>window.open('https://www.instagram.com/akmaljon_azimovv/')}>Instagram</p>
+                <p onClick={()=>window.open('https://twitter.com/akmalcoder/')}>Twitter, </p>
+                <p onClick={()=>window.open('https://t.me/azimjonov_akmaljon')}>Telegram, </p>
+                <p onClick={()=>window.open('https://www.instagram.com/dev.akmal/')}>Instagram</p>
             </LinkDiv>
         </Container>
     </Wrapper>
