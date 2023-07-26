b4ece6a28e651f0b695da79fa2d7c1b391483241
Akmaljon Azimov <96201968+AkmaljonCoder@users.noreply.github.com>
2023-07-26 07:46:07 +0500

    Update Entery.jsx

--- src/components/Entery/Entery.jsx
+++ src/components/Entery/Entery.jsx
@@ -8,7 +8,7 @@ const Entery = () => {
       <Container>
         <TextDiv>
           <h1>Akmal Azimov</h1>
-          <h3>Front-end developer <br/> 17 years old , Uzbekistan</h3>
+          <h3>Front-end developer <br/> 18 years old , Uzbekistan</h3>
           <h2>RU | ENG</h2> 
         </TextDiv>
         <ImageBgc ></ImageBgc>
